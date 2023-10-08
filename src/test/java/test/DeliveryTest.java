9e4009a0670338ce6568410b5462eeee41db5e96
Eldar <ekhismatov@list.ru>
2023-10-08 12:39:38 +0500

    Add Allure

--- src/test/java/test/DeliveryTest.java
+++ src/test/java/test/DeliveryTest.java
@@ -1,9 +1,9 @@
 package test;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
 import data.DataGenerator;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
@@ -15,6 +15,15 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class DeliveryTest {
+    @BeforeAll
+    static void setUpAll(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterAll
+    static void tearDownAll(){
+        SelenideLogger.removeListener("allure");
+    }
+
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
@@ -43,28 +52,6 @@ class DeliveryTest {
         $("[data-test-id=success-notification] .notification__content")
                 .shouldBe(visible, Duration.ofMillis(15000))
                 .shouldHave(exactText("Встреча успешно запланирована на  " + secondMeetingDate));
-    }
-
-    @Test
-    @DisplayName("Should not change the date in the message")
-    void shouldConstantDateInMessage() {
-        var firstMeetingDate = DataGenerator.generateDate(4, 5);
-        var secondMeetingDate = DataGenerator.generateDate(5, 4);
-        $("[data-test-id=city] input").setValue(DataGenerator.generateCity());
-        $("[data-test-id='date'] .input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] .input__control").setValue(firstMeetingDate);
-        $("[name='name']").setValue(DataGenerator.generateName("ru"));
-        $("[name='phone']").setValue(DataGenerator.generatePhone("ru"));
-        $("[data-test-id=agreement]").click();
-        $(withText("Запланировать")).click();
-        $("[data-test-id=success-notification] .notification__content")
-                .shouldBe(visible, Duration.ofMillis(15000))
-                .shouldHave(exactText("Встреча успешно запланирована на  " + firstMeetingDate));
-        $("[data-test-id='date'] .input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] .input__control").setValue(secondMeetingDate);
-        $("[data-test-id=success-notification] .notification__content")
-                .shouldBe(visible, Duration.ofMillis(15000))
-                .shouldHave(exactText("Встреча успешно запланирована на  " + firstMeetingDate));
     }
 
     @Test
@@ -130,21 +117,6 @@ class DeliveryTest {
     }
 
     @Test
-    void shouldComplexName() {
-        var firstMeetingDate = DataGenerator.generateDate(4, 5);
-        $("[data-test-id=city] input").setValue(DataGenerator.generateCity());
-        $("[data-test-id='date'] .input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] .input__control").setValue(firstMeetingDate);
-        $("[name='name']").setValue(DataGenerator.generateComplexName());
-        $("[name='phone']").setValue(DataGenerator.generatePhone("ru"));
-        $("[data-test-id=agreement]").click();
-        $("[class='button__text']").click();
-        $("[data-test-id=success-notification] .notification__content")
-                .shouldBe(visible, Duration.ofMillis(15000))
-                .shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate));
-    }
-
-    @Test
     void shouldLatinLettersInNameField() {
         var firstMeetingDate = DataGenerator.generateDate(4, 5);
         $("[data-test-id=city] input").setValue(DataGenerator.generateCity());
@@ -172,20 +144,6 @@ class DeliveryTest {
     }
 
     @Test
-    void shouldPhoneWithoutPlusSymbol() {
-        var firstMeetingDate = DataGenerator.generateDate(4, 5);
-        $("[data-test-id=city] input").setValue(DataGenerator.generateCity());
-        $("[data-test-id='date'] .input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] .input__control").setValue(firstMeetingDate);
-        $("[name='name']").setValue(DataGenerator.generateName("ru"));
-        $("[name='phone']").setValue("89012345678");
-        $("[data-test-id=agreement]").click();
-        $("[class='button__text']").click();
-        $("[data-test-id='phone'].input_invalid .input__sub")
-                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-    }
-
-    @Test
     void shouldMoreElevenCharactersInPhone() {
         var firstMeetingDate = DataGenerator.generateDate(4, 5);
         $("[data-test-id=city] input").setValue(DataGenerator.generateCity());
@@ -201,34 +159,6 @@ class DeliveryTest {
     }
 
     @Test
-    void shouldLessElevenCharactersInPhone() {
-        var firstMeetingDate = DataGenerator.generateDate(4, 5);
-        $("[data-test-id=city] input").setValue(DataGenerator.generateCity());
-        $("[data-test-id='date'] .input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] .input__control").setValue(firstMeetingDate);
-        $("[name='name']").setValue(DataGenerator.generateName("ru"));
-        $("[name='phone']").setValue("+7901234567");
-        $("[data-test-id=agreement]").click();
-        $("[class='button__text']").click();
-        $("[data-test-id='phone'].input_invalid .input__sub")
-                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-    }
-
-    @Test
-    void shouldOperatorCodeInPhone() {
-        var firstMeetingDate = DataGenerator.generateDate(4, 5);
-        $("[data-test-id=city] input").setValue(DataGenerator.generateCity());
-        $("[data-test-id='date'] .input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] .input__control").setValue(firstMeetingDate);
-        $("[name='name']").setValue(DataGenerator.generateName("ru"));
-        $("[name='phone']").setValue("+70012345678");
-        $("[data-test-id=agreement]").click();
-        $("[class='button__text']").click();
-        $("[data-test-id='phone'].input_invalid .input__sub")
-                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-    }
-
-    @Test
     void shouldCheckboxNotMarked() {
         var firstMeetingDate = DataGenerator.generateDate(4, 5);
         $("[data-test-id=city] input").setValue(DataGenerator.generateCity());
@@ -240,5 +170,6 @@ class DeliveryTest {
         $("[data-test-id=agreement].input_invalid .checkbox__text")
                 .shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
     }
+
 
 }
